69d5d54edd6993b077fa8c6c9ee9b0997e5c1573
Tony <tony@gamil.com>
2017-05-05 18:31:13 +0800

    xkf

--- MaterialTest/app/src/main/java/com/materialtest/xkf/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/materialtest/xkf/materialtest/MainActivity.java
@@ -4,9 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
-
-import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
