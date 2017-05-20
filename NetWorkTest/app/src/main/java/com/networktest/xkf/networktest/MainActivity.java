97bf85cbdf01d9755abba62553e71c6063b58f28
xkf <1131225133@qq.com>
2017-05-20 20:15:03 +0800

    xkf

--- NetWorkTest/app/src/main/java/com/networktest/xkf/networktest/MainActivity.java
+++ NetWorkTest/app/src/main/java/com/networktest/xkf/networktest/MainActivity.java
@@ -2,12 +2,19 @@ package com.networktest.xkf.networktest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view) {
+
     }
 }
