cc5724ce77cef46c890c43364510b96498ee6a55
xkf <1131225133@qq.com>
2017-05-22 18:22:45 +0800

    xkf

---
+++ ServiceTest/app/src/main/java/com/example/xkf/servicetest/MyService.java
@@ -0,0 +1,33 @@
+package com.example.xkf.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+
+public class MyService extends Service {
+    public MyService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
