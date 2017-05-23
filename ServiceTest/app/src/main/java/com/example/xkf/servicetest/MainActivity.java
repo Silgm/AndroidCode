ee8aed3a6e895faf0316bdd11988934a1d08e9dc
xkf <1131225133@qq.com>
2017-05-23 16:40:23 +0800

    xkf

--- ServiceTest/app/src/main/java/com/example/xkf/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/com/example/xkf/servicetest/MainActivity.java
@@ -1,10 +1,36 @@
 package com.example.xkf.servicetest;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private MyService.DownloadBinder downloadBinder;
+
+    //如果想要activity和service通信的话
+    //需要用匿名内部类来继承这个类
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            //这里的service是那个Service的返回对象
+            //要做一个强制类型的转换
+            downloadBinder = (MyService.DownloadBinder) service;
+            //这里处理具体的逻辑
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +39,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startService(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
     }
 
     public void stopService(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
+    }
+
+    public void bindService(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+    }
+
+    public void unbindService(View view) {
+        unbindService(serviceConnection);
     }
 }
--- ServiceTest/app/src/main/java/com/example/xkf/servicetest/MyService.java
+++ ServiceTest/app/src/main/java/com/example/xkf/servicetest/MyService.java
@@ -2,32 +2,55 @@ package com.example.xkf.servicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
 
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
+    private DownloadBinder mBinder = new DownloadBinder();
+    //自己创建一个绑定类
+    //继承Binder
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.e("dalongmao", "startDownload");
+        }
 
-        return null;
+        public int getProgress() {
+            Log.e("dalongmao", "getProgress");
+            return 0;
+        }
     }
 
     @Override
+    public IBinder onBind(Intent intent) {
+        //返回这个类的实例
+        //会在serviceconnect中用到
+        return mBinder;
+    }
+
+    //service运行先是create
+    @Override
     public void onCreate() {
+        Log.e("dalongmao", "onCreate");
         super.onCreate();
     }
 
+    //再是onStartCommand
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e("dalongmao", "onStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
 
+    //销毁运行这个
     @Override
     public void onDestroy() {
+        Log.e("dalongmao", "onDestroy");
         super.onDestroy();
     }
 }
